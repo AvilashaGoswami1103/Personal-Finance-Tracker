e0358c99c9a9572fda9f77441d2d367cb27b3036
Avilasha Goswami <avilashagoswami3@gmail.com>
2026-04-29 20:37:56 +0530

    Enhance comments in calculate_analytics function
    Added comments to clarify the purpose of variables and logic in the calculate_analytics function.

--- backend/services/analytics.py
+++ backend/services/analytics.py
@@ -1,10 +1,13 @@
 def calculate_analytics(transactions):
     total_spending = 0
-    category_breakdown = {}
+    category_breakdown = {}    # An empty dictionary to store spending grouped by category
 
     for t in transactions:
-        total_spending += t["amount"]
+        total_spending += t["amount"]      # Adds the amount field of the current transaction to the running total.
 
+        # Checks if the transaction’s category already exists in the dictionary.
+        # If yes, add the amount to the existing total.
+        # If no, create a new entry with the amount.
         if t.category in category_breakdown:
             category_breakdown[t["category"]] += t["amount"]
         else:
